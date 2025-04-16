05d9420f080d5c475ee150c596bb00e568f7c8e7
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2025-04-16 23:44:29 +0200

    demo showcase, common css, refact

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,25 @@ const sass = require('gulp-sass')(require('sass'));
 
 
 function demo_css() {
-  return src('src/scss/demo_*.scss')
+  return src('src/scss/demo.scss')
     .pipe(sass({
-      loadPaths: [
-        './node_modules/@loltgt/ensemble--demo/src/scss',
-        './node_modules/@loltgt/ensemble-modal--demo/src/scss'
-      ],
       silenceDeprecations: ['import'],
       style: 'compressed'
     }).on('error', sass.logError))
     .pipe(rename(function(srcpath) {
-      srcpath.basename = srcpath.basename.replace('demo_', 'demo-ensemble-');
+      srcpath.basename = srcpath.basename.replace('demo', 'demo-ensemble');
     }))
     .pipe(dest('demo'));
-}
-
-function demo_js() {
-  return src('src/js/demo_*.js')
-  .pipe(rename(function(srcpath) {
-    srcpath.basename = srcpath.basename.replace('demo_', 'demo-ensemble-');
-  }))
-  .pipe(dest('demo'));
 }
 
 const demo_prepublish = series([
   function demo_prepublish() {
     return src(['LICENSE', 'README.md'], {cwd: '../'})
       .pipe(dest('.'));
-  },
-  function demo_prepublish() {
-    return src(['dist/**'], {cwd: '../', base: '.'})
-    .pipe(dest('dist'));
   }
 ]);
 
 
 exports['demo:css'] = demo_css;
-exports['demo:js'] = demo_js;
 exports['demo:prepublish'] = demo_prepublish;
-exports.default = parallel([demo_css, demo_js, demo_prepublish]);
+exports.default = parallel([demo_css, demo_prepublish]);
